de5920b5ad400652d6c81b30df59ab0a8308c451
Sk Md Asib <63531375+Ascarre15@users.noreply.github.com>
2020-10-13 21:10:48 +0530

    Add files via upload

---
+++ app/src/main/java/heaven/on/fire/MainActivity.java
@@ -0,0 +1,13 @@
+package heaven.on.fire;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MenuMain.initModMenu(this);
+    }
+}
